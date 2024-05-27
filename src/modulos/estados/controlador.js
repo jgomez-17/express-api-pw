e60e44809d92a0e60d5456f82df044dc9255199a
jgomez-17 <jgomez.eng@gmail.com>
2024-05-27 17:42:56 -0500

    v27-05-24

--- src/modulos/estados/controlador.js
+++ src/modulos/estados/controlador.js
@@ -210,67 +210,68 @@ module.exports = function(dbInyectada){
     }
 
     // Estado TERMINADO HOY
-async function terminadoHoy(req, res, next) {
-    const estado = 'terminado';
+    async function terminadoHoy(req, res, next) {
+        const estado = 'terminado';
 
-    try {
-        // Consultar todas las órdenes terminadas
-        const ordenesTerminadas = await db.query('ordenes', { estado });
+        try {
+            // Consultar todas las órdenes terminadas
+            const ordenesTerminadas = await db.query('ordenes', { estado });
 
-        // Verificar si hay órdenes terminadas
-        if (!ordenesTerminadas || ordenesTerminadas.length === 0) {
-            return res.status(404).json({ mensaje: 'No se encontraron órdenes terminadas' });
-        }
-
-        // Obtener la fecha actual en formato YYYY-MM-DD
-        const today = format(new Date(), 'yyyy-MM-dd');
-
-        const respuesta = {
-            ordenes: [],
-            totalRecaudado: 0,
-            numeroOrdenesHoy: 0
-        };
-
-        // Consultar los datos relacionados para cada orden terminada y filtrar por fecha actual
-        for (const orden of ordenesTerminadas) {
-            const fechaOrden = format(new Date(orden.fecha_orden), 'yyyy-MM-dd');
-            if (fechaOrden === today) {
-                // Consultar datos relacionados del cliente
-                const cliente = await db.uno('clientes', orden.cliente_id);
-
-                // Consultar datos relacionados del vehículo
-                const vehiculo = await db.uno('vehiculos', orden.vehiculo_id);
-
-                // Consultar datos relacionados del servicio
-                const servicio = await db.uno('servicios', orden.servicio_id);
-
-                respuesta.ordenes.push({
-                    id: orden.id,
-                    fechaOrden: orden.fecha_orden,
-                    estado: orden.estado,
-                    cliente: cliente[0],
-                    vehiculo: vehiculo[0],
-                    servicio: servicio[0]
-                }) 
-                // Sumar el costo del servicio al total recaudado
-                respuesta.totalRecaudado += parseFloat(servicio[0].costo);
-                respuesta.numeroOrdenesHoy += 1;
+            // Verificar si hay órdenes terminadas
+            if (!ordenesTerminadas || ordenesTerminadas.length === 0) {
+                return res.status(404).json({ mensaje: 'No se encontraron órdenes terminadas' });
             }
-        }
 
-        // Verificar si hay órdenes terminadas para el día actual
-        if (respuesta.ordenes.length === 0) {
-            return res.status(404).json({ mensaje: 'No se encontraron órdenes terminadas para el día actual' });
-        }
+            // Obtener la fecha actual en formato YYYY-MM-DD
+            const today = format(new Date(), 'yyyy-MM-dd');
 
-        // Enviar la respuesta con las órdenes terminadas del día actual y sus datos relacionados
-        res.status(200).json(respuesta);
-        console.log(respuesta);
-    } catch (error) {
-        console.error('Error al consultar la información de las órdenes terminadas:', error);
-        next(error);
+            const respuesta = {
+                ordenes: [],
+                totalRecaudado: 0,
+                numeroOrdenesHoy: 0
+            };
+
+            // Consultar los datos relacionados para cada orden terminada y filtrar por fecha actual
+            for (const orden of ordenesTerminadas) {
+                const fechaOrden = format(new Date(orden.fecha_orden), 'yyyy-MM-dd');
+                if (fechaOrden === today) {
+                    // Consultar datos relacionados del cliente
+                    const cliente = await db.uno('clientes', orden.cliente_id);
+
+                    // Consultar datos relacionados del vehículo
+                    const vehiculo = await db.uno('vehiculos', orden.vehiculo_id);
+
+                    // Consultar datos relacionados del servicio
+                    const servicio = await db.uno('servicios', orden.servicio_id);
+
+                    respuesta.ordenes.push({
+                        id: orden.id,
+                        fechaOrden: orden.fecha_orden,
+                        estado: orden.estado,
+                        empleado: orden.empleado,
+                        cliente: cliente[0],
+                        vehiculo: vehiculo[0],
+                        servicio: servicio[0]
+                    }) 
+                    // Sumar el costo del servicio al total recaudado
+                    respuesta.totalRecaudado += parseFloat(servicio[0].costo);
+                    respuesta.numeroOrdenesHoy += 1;
+                }
+            }
+
+            // Verificar si hay órdenes terminadas para el día actual
+            if (respuesta.ordenes.length === 0) {
+                return res.status(404).json({ mensaje: 'No se encontraron órdenes terminadas para el día actual' });
+            }
+
+            // Enviar la respuesta con las órdenes terminadas del día actual y sus datos relacionados
+            res.status(200).json(respuesta);
+            console.log(respuesta);
+        } catch (error) {
+            console.error('Error al consultar la información de las órdenes terminadas:', error);
+            next(error);
+        }
     }
-}
     
 
     //en las ordenes no necesito borrarlas ni editarlas
