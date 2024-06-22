d4f0b6d2dff26c5f7a64395415961310bbe593e8
jgomez-17 <jgomez.eng@gmail.com>
2024-06-21 22:09:07 -0500

    delete mysql

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,91 +1,99 @@
-const mysql = require('mysql');
-const config = require('../config');
-const { error } = require('../red/respuestas');
+// const mysql = require('mysql');
+// const config = require('../config');
+// const { error } = require('../red/respuestas');
 
 
-const dbconfig = {
-    host: config.mysql.host,
-    user: config.mysql.user,
-    password: config.mysql.password,
-    database: config.mysql.database
-}
+// const dbconfig = {
+//     host: config.mysql.host,
+//     user: config.mysql.user,
+//     password: config.mysql.password,
+//     database: config.mysql.database
+// }
 
-let conexion;
+// let conexion;
 
-function conMysql(){
-    conexion = mysql.createConnection(dbconfig);
+// function conMysql(){
+//     conexion = mysql.createConnection(dbconfig);
 
-    conexion.connect((err) => {
-        if(err) {
-            console.log('[db err]', err);
-            setTimeout(conMysql, 200);
-        } else {
-            console.log('Database Conected!!!')
-        }
-    })
+//     conexion.connect((err) => {
+//         if(err) {
+//             console.log('[db err]', err);
+//             setTimeout(conMysql, 200);
+//         } else {
+//             console.log('Database Conected!!!')
+//         }
+//     })
 
-    conexion.on('error', err => {
-        console.log('[db err]', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-            conMysql();
-        } else {
-            throw err;
-        }
-    })
-}
+//     conexion.on('error', err => {
+//         console.log('[db err]', err);
+//         if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//             conMysql();
+//         } else {
+//             throw err;
+//         }
+//     })
+// }
 
-conMysql();
+// conMysql();
 
-function todos (tabla) {
-    return new Promise((resolve, reject) => {
-        conexion.query(`SELECT * FROM ${tabla}`, (error, result) => {
-            return error ? reject(error) : resolve(result);
-       })   
-    });
-}
+// function todos (tabla) {
+//     return new Promise((resolve, reject) => {
+//         conexion.query(`SELECT * FROM ${tabla}`, (error, result) => {
+//             return error ? reject(error) : resolve(result);
+//        })   
+//     });
+// }
 
-function uno (tabla, id) {
-    return new Promise((resolve, reject) => {
-        conexion.query(`SELECT * FROM ${tabla} WHERE id=${id}`, (error, result) => {
-            return error ? reject(error) : resolve(result);   
-        })   
-     });
-}
+// function uno (tabla, id) {
+//     return new Promise((resolve, reject) => {
+//         conexion.query(`SELECT * FROM ${tabla} WHERE id=${id}`, (error, result) => {
+//             return error ? reject(error) : resolve(result);   
+//         })   
+//      });
+// }
 
-function agregar (tabla, data) {
-    return new Promise((resolve, reject) => {
-        conexion.query(`INSERT INTO ${tabla} SET ? ON DUPLICATE KEY UPDATE ?`, [data,data], (error, result) => {
-            return error ? reject(error) : resolve(result);   
-        })   
-     });
-}
+// function agregar (tabla, data) {
+//     return new Promise((resolve, reject) => {
+//         conexion.query(`INSERT INTO ${tabla} SET ? ON DUPLICATE KEY UPDATE ?`, [data,data], (error, result) => {
+//             return error ? reject(error) : resolve(result);   
+//         })   
+//      });
+// }
 
 
-function eliminar (tabla, data) {
-    return new Promise((resolve, reject) => {
-        conexion.query(`DELETE FROM ${tabla} WHERE id = ?`, data.id, (error, result) => {
-            return error ? reject(error) : resolve(result);   
-        })   
-     });
-}
+// function eliminar (tabla, data) {
+//     return new Promise((resolve, reject) => {
+//         conexion.query(`DELETE FROM ${tabla} WHERE id = ?`, data.id, (error, result) => {
+//             return error ? reject(error) : resolve(result);   
+//         })   
+//      });
+// }
 
- function query (tabla, consulta) {
-     return new Promise((resolve, reject) => {
-         conexion.query(`SELECT * FROM ${tabla} WHERE ?`, consulta, (error, result) => {
-             return error ? reject(error) : resolve(result[0]);   
-         })   
-      });
- }
+//  function query (tabla, consulta) {
+//      return new Promise((resolve, reject) => {
+//          conexion.query(`SELECT * FROM ${tabla} WHERE ?`, consulta, (error, result) => {
+//              return error ? reject(error) : resolve(result[0]);   
+//          })   
+//       });
+//  }
 
-  function queryEstados (tabla, consulta) {
-     return new Promise((resolve, reject) => {
-         conexion.query(`SELECT * FROM ${tabla} WHERE ?`, consulta, (error, result) => {
-             return error ? reject(error) : resolve(result);   
-         })   
-      });
- }
+//   function queryEstados (tabla, consulta) {
+//      return new Promise((resolve, reject) => {
+//          conexion.query(`SELECT * FROM ${tabla} WHERE ?`, consulta, (error, result) => {
+//              return error ? reject(error) : resolve(result);   
+//          })   
+//       });
+//  }
 
-// function actualizar(tabla, datosActualizados, condicion) {
+// // function actualizar(tabla, datosActualizados, condicion) {
+// //     return new Promise((resolve, reject) => {
+// //         conexion.query(`UPDATE ${tabla} SET ? WHERE ?`, [datosActualizados, condicion], (error, result) => {
+// //             return error ? reject(error) : resolve(result);
+// //         });
+// //     });
+// // }
+
+// function actualizarEstadoOrden(tabla, datosActualizados, condicion) {
 //     return new Promise((resolve, reject) => {
 //         conexion.query(`UPDATE ${tabla} SET ? WHERE ?`, [datosActualizados, condicion], (error, result) => {
 //             return error ? reject(error) : resolve(result);
@@ -93,43 +101,35 @@ function eliminar (tabla, data) {
 //     });
 // }
 
-function actualizarEstadoOrden(tabla, datosActualizados, condicion) {
-    return new Promise((resolve, reject) => {
-        conexion.query(`UPDATE ${tabla} SET ? WHERE ?`, [datosActualizados, condicion], (error, result) => {
-            return error ? reject(error) : resolve(result);
-        });
-    });
-}
+// // Nueva función para consultas generales
+// function queryGeneral(sql, params) {
+//     return new Promise((resolve, reject) => {
+//         conexion.query(sql, params, (error, result) => {
+//             return error ? reject(error) : resolve(result);
+//         });
+//     });
+// }
 
-// Nueva función para consultas generales
-function queryGeneral(sql, params) {
-    return new Promise((resolve, reject) => {
-        conexion.query(sql, params, (error, result) => {
-            return error ? reject(error) : resolve(result);
-        });
-    });
-}
-
-// Nueva función para buscar por usuario
-function buscarPorUsuario(tabla, usuario) {
-    return new Promise((resolve, reject) => {
-        const query = `SELECT * FROM ${tabla} WHERE usuario = ?`;
-        conexion.query(query, [usuario], (error, result) => {
-            return error ? reject(error) : resolve(result[0]);
-        });
-    });
-}
+// // Nueva función para buscar por usuario
+// function buscarPorUsuario(tabla, usuario) {
+//     return new Promise((resolve, reject) => {
+//         const query = `SELECT * FROM ${tabla} WHERE usuario = ?`;
+//         conexion.query(query, [usuario], (error, result) => {
+//             return error ? reject(error) : resolve(result[0]);
+//         });
+//     });
+// }
 
 
 
-module.exports = {
-    todos,
-    uno,
-    agregar,
-    eliminar,
-    query,
-    queryEstados,
-    actualizarEstadoOrden,
-    queryGeneral,
-    buscarPorUsuario
-}
+// module.exports = {
+//     todos,
+//     uno,
+//     agregar,
+//     eliminar,
+//     query,
+//     queryEstados,
+//     actualizarEstadoOrden,
+//     queryGeneral,
+//     buscarPorUsuario
+// }
